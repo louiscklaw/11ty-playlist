eaf8587b8350cde35c705ea4b3d740db9ba641fe
louiscklaw <louiscklaw@users.noreply.github.com>
2023-02-28 01:29:25 +0800

    update eleventy-base-blog-helloworld,

--- eleventy-base-blog-helloworld/scripts/rebuild_prod.sh
+++ eleventy-base-blog-helloworld/scripts/rebuild_prod.sh
@@ -7,7 +7,7 @@ git pull
 docker compose -f docker/docker-compose.prod.yml pull
 docker compose -f docker/docker-compose.prod.yml build
 
-docker compose -f docker/docker-compose.prod.yml kill -f
+docker compose -f docker/docker-compose.prod.yml kill
 docker compose -f docker/docker-compose.prod.yml down
 
 docker compose -f docker/docker-compose.prod.yml up -d 
