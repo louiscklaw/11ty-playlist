fb643839ad152302c90506c1bc876aa70083612c
louiscklaw <louiscklaw@users.noreply.github.com>
2023-03-15 00:05:19 +0800

    update i18n,

--- eleventy-base-blog-papermod-helloworld/app/_data/i18n/index.js
+++ eleventy-base-blog-papermod-helloworld/app/_data/i18n/index.js
@@ -3,6 +3,16 @@ module.exports = {
     'en-GB': 'Hello',
     'es-ES': 'Hola'
   },
+  Works: {
+    'en-GB': 'Works',
+    'es-ES': 'Hola',
+    'zh-HK': '手作仔'
+  },  Search: {
+    'zh-HK': '搵嘢'
+  },
+  Tags: {
+    'zh-HK': '標纖'
+  },
   hello_name: {
     'en-GB': 'Hello, {{ name }}!',
     'es-ES': '¡Hola {{ name }}!'
