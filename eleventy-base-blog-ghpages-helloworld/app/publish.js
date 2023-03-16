f7aea9a6a8f656f4dae6d040e693616c5d35acae
louiscklaw <logickee@gmail.com>
2023-03-17 02:36:34 +0800

    update format adopt window,

--- eleventy-base-blog-ghpages-helloworld/app/publish.js
+++ eleventy-base-blog-ghpages-helloworld/app/publish.js
@@ -1,10 +1,14 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish('_site', {
-  branch: 'master',
-  repo: 'https://github.com/louiscklaw/portfolio-11ty.git'
-},function(err) {
-  console.log(err)
-});
+ghpages.publish(
+  '_site',
+  {
+    branch: 'master',
+    repo: 'https://github.com/louiscklaw/portfolio-11ty.git',
+  },
+  function (err) {
+    console.log(err);
+  },
+);
 
-console.log('publish done')
+console.log('publish done');
