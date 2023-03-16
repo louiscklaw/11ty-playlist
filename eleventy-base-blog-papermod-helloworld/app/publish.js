7c377cce32831e9a095bff107681d15c537b9d9d
louiscklaw <logickee@gmail.com>
2023-03-17 03:29:51 +0800

    update,

---
+++ eleventy-base-blog-papermod-helloworld/app/publish.js
@@ -0,0 +1,14 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+  '_site',
+  {
+    branch: 'master',
+    repo: 'https://github.com/louiscklaw/portfolio-11ty.git',
+  },
+  function (err) {
+    console.log(err);
+  },
+);
+
+console.log('publish done');
