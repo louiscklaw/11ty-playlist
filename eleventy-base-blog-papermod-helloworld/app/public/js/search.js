9c7f2881dc8a1f5f7d2405bb9a11d284ceb062c9
louiscklaw <louiscklaw@users.noreply.github.com>
2023-03-18 16:42:04 +0800

    update eleventy-base-blog-helloworld,

--- eleventy-base-blog-papermod-helloworld/app/public/js/search.js
+++ eleventy-base-blog-papermod-helloworld/app/public/js/search.js
@@ -2,33 +2,40 @@
   'use strict';
 
   const search = e => {
-    const results = window.searchIndex.search(e.target.value, {
-      bool: 'OR',
-      expand: true,
-    });
-
-    const resTitleEl = document.getElementById('result-title');
-
+    console.log(e.target.value.length == 0)
 
     const resEl = document.getElementById('searchResults');
+    const resTitleEl = document.getElementById('result-title');
     const noResultsEl = document.getElementById('noResultsFound');
 
-    resEl.innerHTML = '';
+    if (e.target.value.length == 0){
 
-    if (results) {
-      // show result
+      resEl.style.display = 'none';
+      resTitleEl.style.display='none';
       noResultsEl.style.display = 'none';
-      resTitleEl.style.display='block'
+    }else{
 
-      results.map(r => {
-        const { id, title, description } = r.doc;
-        const liEl = document.createElement('li');
-        liEl.classList.add('basis-1/3')
-        liEl.classList.add('px-2')
-        resEl.appendChild(liEl);
+      const results = window.searchIndex.search(e.target.value, {
+        bool: 'OR',
+        expand: true,
+      });
 
-        const div = document.createElement('div');
-        div.innerHTML=
+      resEl.innerHTML = '';
+
+      if (results) {
+        // show result
+        noResultsEl.style.display = 'none';
+        resTitleEl.style.display='block'
+
+        results.map(r => {
+          const { id, title, description } = r.doc;
+          const liEl = document.createElement('li');
+          liEl.classList.add('basis-1/3')
+          liEl.classList.add('px-2')
+          resEl.appendChild(liEl);
+
+          const div = document.createElement('div');
+          div.innerHTML=
 `
 <a href="${id}">
   <div class="
@@ -48,19 +55,23 @@
   </div>
 </a>
 `
-        liEl.appendChild(div);
-      });
-    } else {
-      // hide result
-      noResultsEl.style.display = 'block';
-      resTitleEl.style.display='none'
+          liEl.appendChild(div);
+        });
+      } else {
+        // hide result
+        noResultsEl.style.display = 'block';
+        resTitleEl.style.display='none'
+      }
     }
+
+
   };
 
   fetch('/search-index.json').then(response =>
     response.json().then(rawIndex => {
       window.searchIndex = elasticlunr.Index.load(rawIndex);
-      document.getElementById('searchField').addEventListener('input', search);
+      document.getElementById('searchField')
+        .addEventListener('input', search);
     }),
   );
 })(window, document);
